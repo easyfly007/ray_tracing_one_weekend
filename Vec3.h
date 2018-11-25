52492a9d820aa1cd6df726ec394fd5c39db29464
easyfly007 <easyfly.huang@gmail.com>
2018-11-25 19:40:03 +0800

    add #define for header file

--- Vec3.h
+++ Vec3.h
@@ -1,3 +1,6 @@
+#ifndef __VEC3__
+#define __VEC3__
+
 #include <math.h>
 #include <stdlib.h>
 #include <iostream>
@@ -113,3 +116,5 @@ inline Vec3 corss (const Vec3 &v1, const Vec3 &v2){
             (-(v1.e[0] * v2.e[2] - v1.e[2] * v2.e[0])),
             (v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]));
 }
+
+#endif
