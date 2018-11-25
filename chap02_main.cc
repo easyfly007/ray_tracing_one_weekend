aa8ec3a596b46e6dff3ef9f55ab0478ba03a2cec
easyfly007 <easyfly.huang@gmail.com>
2018-11-25 16:43:24 +0800

    add chap02 codes

---
+++ chap02_main.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Vec3.h"
+using namespace std;
+
+int main(){
+	int nx = 200;
+	int ny = 100;
+	cout << "P3\n" << nx << " " << ny << "\n255\n";
+	for (int j = ny-1; j >=0; j --)
+	{
+
+		for (int i = 0; i < nx; i ++){
+			Vec3 col(float(i)/ float(ny), float(j) / float(ny), 0.2);
+			int ir = int(255.99 * col[0]);
+			int ig = int(255.99 * col[1]);
+			int ib = int(255.99 * col[2]);
+			cout << ir << " " << ig << " " << ib << endl;
+		}
+	}
+	return 0;
+}
