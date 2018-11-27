9d2a7c81413cb62f7ae4832acf699260ed9b6a57
easyfly007 <easyfly.huang@gmail.com>
2018-11-27 21:00:29 +0800

    add ray header file

---
+++ Ray.h
@@ -0,0 +1,16 @@
+#ifndef __RAY__
+#define __RAY__
+
+#include "Vec3.h"
+class Ray
+{
+public:
+	Ray(){}
+	Vec3 origin() const {return A;}
+	Vec3 direction() const {return B;}
+	Vec3 point_at_parameter(float t) const{return  A + t * B;}
+	Vec3 A;
+	Vec3 B;
+};
+
+#endif
