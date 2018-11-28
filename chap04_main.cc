f29509f349059a3697f4e31bb37a21d820f7da73
easyfly007 <easyfly.huang@gmail.com>
2018-11-28 20:13:06 +0800

    add chap04 code

---
+++ chap04_main.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "Ray.h"
+
+using namespace std;
+
+Vec3 color(const Ray & r)
+{
+	Vec3 unit_direction = unit_vector(r.unit_direction());
+	float t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0);
+}
+
+int main()
+{
+	int nx = 200;
+	int ny = 100;
+	cout << " P3\n" << nx << " " << ny << "\n255\n";
+	Vec3 lower_left_corner(-2.0, -1.0, -1.0);
+	Vec3 horizontal(4.0, 2.0, 0.0);
+	Vec3 vertical(0.0, 2.0, 0.0);
+	Vec3 origin(0.0, 0.0, 0.0);
+
+	for (int j = ny -1; j >= 0; j--)
+	{
+		for (int i = 0; i < nx; i ++)
+		{
+			float u = float(i) / float(nx);
+			float v = float(j) / float(ny);
+			Ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+			Vec3 col = color(r);
+			int ir = int(255.99 * col[0]);
+			int ig = int(255.99 * col[1]);
+			int ib = int(255.99 * col[2]);
+
+			cout << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+	return 0;
+}
